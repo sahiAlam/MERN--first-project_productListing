95e0736b4f67c32772369db08e36beb625f8744a
MD SAHI ALAM KHAN <ksahi4088@gmail.com>
2023-06-08 13:39:12 +0530

    vercel config uploaded

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import productRouter from "./routes/product.js";
+
 export const app = express();
 dotenv.config();
 
 // using middlewares
 app.use(express.json());
-app.use(express.static("build"));
+app.use(express.static(path.resolve(path.join("build"))));
 app.use(cors());
 app.use("/api/v1/products", productRouter);
